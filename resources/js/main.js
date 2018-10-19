1f19aa6fb1a129177fb83fa9842ba13e865fde3f
Edward Gorbachev <edwardgorbachev@gmail.com>
2018-10-19 05:24:34 +0300

    stage works by comments

--- resources/js/main.js
+++ resources/js/main.js
@@ -30,4 +30,43 @@ $(function(){
     //
     // });
 
+
+    scaleMobileContent();
+    //window.addEventListener('orientationchange', scaleMobileContent);
+
 });
+
+function scaleMobileContent(){
+
+    console.log('CHANGE ORIENTATION');
+
+    var width = 320;
+    var screen_width = screen.width;
+    var mobile_width = 500;
+    var mobile_width_max = 600;
+    var scale = '1';
+
+    if(screen_width <= width ){
+        scale = screen_width / parseFloat(mobile_width);
+        scale = scale.toFixed(2);
+        addMetaViewport( scale );
+    }
+    else if ( screen_width <= mobile_width_max ) {
+        scale = screen_width / parseFloat(mobile_width);
+        scale = scale.toFixed(2);
+        addMetaViewport( scale );
+    }
+    else
+    {
+        addMetaViewport( scale );
+    }
+}
+
+function addMetaViewport( scale )
+{
+    viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute('content', 'width=device-width, initial-scale='+scale);
+
+    //$('head').append('<meta name="viewport" content="width=device-width, initial-scale="' + scale +'">');
+
+}
